262ee86a0c7e669ce50f2708b2658ca7f822a7cb
NicholasBoltney <148574215+NicholasBoltney@users.noreply.github.com>
2023-10-28 13:03:14 +0200

    Add files via upload

--- Calculate.js
+++ Calculate.js
@@ -1,9 +1,25 @@
 let weightInput = document.getElementById("weightInput");
 let heightInput = document.getElementById("heightInput");
+let score = 0;
+let scoreText = document.getElementById("ScoreText");
 
 function CalculateBMI()
 {
-  console.log(weightInput.value);
-  console.log(heightInput.value);
-  console.log(weightInput.value/(heightInput.value * heightInput.value));
+  score = weightInput.value/(heightInput.value * heightInput.value);
+
+  if(isNaN(score))
+  {
+    if(isNaN(weightInput.value))
+    {
+      weightInput.style.color = "darkred";
+    }
+  }
+  else{
+    scoreText.innerHTML = score.toFixed(2);
+    weightInput.style.color = "rgb(88, 88, 88)";
+  }
+
+ 
+
+  event.preventDefault();
 }
